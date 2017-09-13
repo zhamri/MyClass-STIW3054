b36324b952aabb75c5a2570f2794f15477262bb1
Zhamri Che Ani <zhamri@gmail.com>
2017-09-13 11:45:32 +0800

    Example-03

---
+++ MyThreadThread.java
@@ -0,0 +1,22 @@
+
+package Week_01;
+
+public class MyThreadThread extends Thread {
+
+    public static void main(String[] args) {
+        new Thread(new MySleep()).start();
+        new Thread(new MySleep()).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int x = 0; x < 1000; x++) {
+                System.out.println(x);
+                sleep(2000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
